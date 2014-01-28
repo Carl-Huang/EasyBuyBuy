20b70fc2831bb8c4a95b7a00561aa6612231d4a0
Carl <Carl@Carls-MacBook-Pro.local>
2014-01-28 16:42:33 +0800

    project files

---
+++ EasyBuyBuy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EasyBuyBuy
+//
+//  Created by Carl on 14-1-27.
+//  Copyright (c) 2014年 helloworld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
